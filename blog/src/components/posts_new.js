3701b58d68b18e686a71e3b6cf28e62d713960ae
Adrianacmy <iamadrianachen@gmail.com>
2018-06-18 11:30:14 -0700

    add dummy postsNewForm

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class PostsNew extends React.Component{
+class PostsNew extends React.Component{
+
+  renderField (field){
+    return (
+      <div className="form-group">
+        <input 
+        type="text"
+        className="form-control"
+        placeholder={field.placeholder}
+        {...field.input}
+        />
+      </div>
+    );
+  }
+
   render(){
     return (
-      <div>add a post</div>
+      <form>
+        <Field 
+          placeholder="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field 
+          placeholder="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+         <Field 
+          placeholder="Content"
+          name="content"
+          component={this.renderField}
+        />
+      
+      </form>
     );
   }
 }
 
 
+export default reduxForm({
+  form: 'PostsNewForm'
+}) (PostsNew);
