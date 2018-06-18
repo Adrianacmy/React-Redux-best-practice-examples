c0380ec9f874a2bad2d3be2ac4b86cd7ba71a3f6
Adrianacmy <iamadrianachen@gmail.com>
2018-06-18 12:02:17 -0700

    handle postsNewForm submit

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -12,13 +12,22 @@ class PostsNew extends React.Component{
         placeholder={field.placeholder}
         {...field.input}
         />
+        {field.meta.touched ? field.meta.error : ''}
       </div>
     );
   }
 
+  onSubmit(values){
+    // this ==== component
+    console.log(values);
+  }
+
   render(){
+
+    const {handleSubmit} = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field 
           placeholder="Title"
           name="title"
@@ -34,13 +43,32 @@ class PostsNew extends React.Component{
           name="content"
           component={this.renderField}
         />
-      
+        <button className="btn bnt-primary">Submit</button>
       </form>
     );
   }
 }
 
+function validate_post(values){
+  const errors = {};
+
+  if(!values.title){
+    errors.title = "Enter a title";
+  }
+
+  if(!values.categories){
+    errors.categories = "Enter a category";
+  }
+
+  if(!values.content){
+    errors.content = "Enter some content";
+  }
+
+  return errors;
+}
+
 
 export default reduxForm({
+  validate: validate_post,
   form: 'PostsNewForm'
 }) (PostsNew);
