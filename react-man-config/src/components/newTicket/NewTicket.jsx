1ab04c8fe89b87d652f0b2c91934b4ac5efe3a30
Adrianacmy <iamadrianachen@gmail.com>
2018-06-27 15:04:34 -0700

    add handClick in newTicket

--- react-man-config/src/components/newTicket/NewTicket.jsx
+++ react-man-config/src/components/newTicket/NewTicket.jsx
@@ -31,25 +31,20 @@ class NewTicket extends React.Component{
     this.state = {
       formVisible: false
     };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState({formVisible: true});
+    // console.log('current formVisible ' + this.state.formVisible);
   }
 
   render(){
     return (
       <div>
-        <form>
-          <input 
-            type='text'
-            id='names'
-            placeholder='pair name'/>
-          <input
-            type='text'
-            id='location'
-            placeholder='Location'/>
-          <textarea
-            id='issue'
-            placeholder='Describe your issue.'/>
-          <button type='submit'>Help!</button>
-        </form>
+        <p>This is about to be a new ticket</p>
+        <p onClick={this.handleClick}>Click me to show a form</p>
       </div>
     );
   }
